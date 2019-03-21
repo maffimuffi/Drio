0512e6e233da4a7ed873473c482fe82a9897c10e
joonatri <33051433+joonatri@users.noreply.github.com>
2019-03-21 12:32:54 +0200

    Pelaajanvaihto ja pelaajien lisääminen

--- Drio/Assets/Scripts/CharacterMovement.cs
+++ Drio/Assets/Scripts/CharacterMovement.cs
@@ -8,15 +8,19 @@ public class CharacterMovement : MonoBehaviour
     float moveSpeed = 0.2f;
     float jumpSpeed = 150f;
     private float rotateSpeed = 5f;
-    private GameObject playerTransform;
+    public GameObject playerTransform;
     private bool jumpedCounter = false;
     private bool jumped = false;
     private float timer = 0;
+    private bool doubleJump;
+    
+    
+   
     
     // Start is called before the first frame update
     void Start()
     {       
-        playerTransform = GameObject.FindWithTag("PlayerTransformPoint");
+        
     }
 
     // Update is called once per frame
@@ -54,26 +58,49 @@ public class CharacterMovement : MonoBehaviour
                 jumpedCounter = false;
             }
         }
+        
         //Debuggaus speed yms
         //Debug.Log("Rigidbody speed: " + gameObject.GetComponent<Rigidbody>().velocity.magnitude + " jumped: " + jumped);
-        if (jumped && gameObject.GetComponent<Rigidbody>().velocity.magnitude >= 0 && gameObject.GetComponent<Rigidbody>().velocity.magnitude <= 0.5 )
+        if (jumped && gameObject.GetComponent<Rigidbody>().velocity.magnitude >= 0 && gameObject.GetComponent<Rigidbody>().velocity.magnitude <= 0.5)
         { //impulse huono. näyttää epärealistiselta.
             gameObject.GetComponent<Rigidbody>().AddForce(Vector3.down * (jumpSpeed / 2), ForceMode.Impulse);
             jumped = false;
         }
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //not working
             Debug.DrawRay(transform.position,Vector3.down,Color.green,0.5f);
             RaycastHit ground;
             
-            if (Physics.Raycast(transform.position, Vector3.down, out ground, 0.5f))
+            if (Physics.Raycast(transform.position, Vector3.down, out ground, 0.5f)  || doubleJump)
             {
+            
                 
                 gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
                 
                 jumpedCounter = true;
+                if (PlayerChanger.CharacterSelect == 1)
+                {
+                    if (doubleJump) 
+                    {doubleJump = false;} else
+                    if (!doubleJump)
+                    {
+                        doubleJump = true;
+
+                    }
+                }
+                
             }
         }
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Terrain"))
+        {
+            doubleJump = false;
+        } 
     }
 }
---
+++ Drio/Assets/Scripts/PlayerChanger.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerChanger : MonoBehaviour
+{
+    ///<see cref="1=Wind,2=Earth,3=Fire"/>
+    public static int CharacterSelect;
+
+    public GameObject WindCamera;
+    public GameObject EarthCamera;
+    public GameObject FireCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+     SetCamera();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ChangePlayer(1);
+        } else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ChangePlayer(2);
+        }else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ChangePlayer(3);
+        }
+    }
+    ///<see cref="ChangePlayer vaihtaa pelaajaa. playerSelect 1=Wind,2=Earth,3=Fire"/>
+    public void ChangePlayer(int playerSelect)
+    {
+        CharacterSelect = playerSelect;
+        SetCamera();
+    }
+
+    public void SetCamera()
+    {
+        if (CharacterSelect == 1)
+        {
+            EarthCamera.SetActive(false);
+            FireCamera.SetActive(false);
+            WindCamera.SetActive(true);
+            
+        } else if (CharacterSelect == 2)
+        {
+            EarthCamera.SetActive(true);
+            FireCamera.SetActive(false);
+            WindCamera.SetActive(false);
+        } else if (CharacterSelect == 3)
+        {
+            EarthCamera.SetActive(false);
+            FireCamera.SetActive(true);
+            WindCamera.SetActive(false);
+        }
+        else
+        {
+            CharacterSelect = 1;
+            EarthCamera.SetActive(false);
+            FireCamera.SetActive(false);
+            WindCamera.SetActive(true);
+        }
+    }
+}
