e4ad5edb33bf45dd9a905dd1ab9a0a46ebafad06
DESKTOP-0B0RDJ4\mrme_ <anttti.nyholm@gmail.com>
2019-03-26 10:20:04 +0200

    a

---
+++ Drio/Assets/Scripts/Boulder.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boulder : MonoBehaviour
+{
+
+    public bool move;
+    float moveSpeed = 0.005f;
+    public GameObject player;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        if(move == true)
+        {
+            transform.position += player.transform.forward * moveSpeed;
+        }
+        
+    }
+
+
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("EarthDragon"))
+        {
+            move = true;
+        }
+
+        else if(!other.collider.CompareTag("EarthDragon"))
+        {
+            move = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (move == true)
+        {
+            rb.AddRelativeForce(Vector3.forward * moveSpeed);
+        }
+        
+    }
+
+}
+
+
+
---
+++ Drio/Assets/Scripts/TestMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    public CharacterController controller;
+
+    private Vector3 movement;
+
+    public float gravityScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float yStore = movement.y;
+        movement = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
+        movement = movement.normalized * moveSpeed;
+        movement.y = yStore;
+
+        if (controller.isGrounded)
+        {
+            movement.y = 0f;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                movement.y = jumpForce;
+            }
+        }
+
+        movement.y = movement.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+
+        controller.Move(movement * Time.deltaTime);
+    }
+
+   
+}
