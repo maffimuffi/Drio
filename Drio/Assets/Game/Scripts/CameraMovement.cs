23e86699b5a03c7ef213948d4a295f566f83b3ac
DESKTOP-0B0RDJ4\mrme_ <anttti.nyholm@gmail.com>
2019-04-03 14:35:35 +0300

    CamLock

--- Drio/Assets/Game/Scripts/CameraMovement.cs
+++ Drio/Assets/Game/Scripts/CameraMovement.cs
@@ -13,9 +13,9 @@ public class CameraMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
-        
-      
+
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Update is called once per frame
