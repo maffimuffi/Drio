2ae710abdf41bbffadf747fa702b20bab2f35fa7
joonatri <33051433+joonatri@users.noreply.github.com>
2019-04-17 11:34:28 +0300

    ui text changes

---
+++ Drio/Assets/Game/Scripts/UIColliderTriggerTextChanger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIColliderTriggerTextChanger : MonoBehaviour
+{
+    public int changerText;
+    private UITextPopup uiText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        uiText = GameObject.Find("TextPopup").GetComponent<UITextPopup>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            uiText.TextChange(changerText);
+            uiText.EnterSite();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            uiText.ExitSite();
+        }
+    }
+}
---
+++ Drio/Assets/Game/Scripts/UITextPopup.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextPopup : MonoBehaviour
+{
+    private RawImage backroundMat;
+    private bool entering;
+    private bool entered;
+    public float popupCounter;
+    public float maxTime;
+    public float popupSpeed;
+    public float maxTransperency;
+    public float transperencyChangeSpeed;
+    private bool exiting;
+    public TextMeshProUGUI text;
+    public float maxSize;
+
+    public float minSize;
+
+    public float transperency;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        popupCounter = 0;
+        backroundMat = GetComponent<RawImage>();
+        text = GetComponentInChildren<TextMeshProUGUI>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transperency = backroundMat.color.a;
+        if (entering && !exiting)
+        {
+            popupCounter += Time.deltaTime;
+            if (popupCounter >= maxTime)
+            {
+                entering = false;
+                entered = true;
+                popupCounter = 0;
+                
+            }
+        }
+
+        if (exiting && (entering || entered))
+        {
+            popupCounter -= Time.deltaTime;
+            if (popupCounter <= 0)
+            {
+                entered = false;
+                entering = false;
+                exiting = false;
+                popupCounter = 0;
+            }
+        } 
+    }
+
+    public void EnterSite()
+    {
+        entering = true;
+        exiting = false;
+    }
+
+    public void TextChange(int x)
+    {
+        if (x == 0)
+        {
+            text.text = "Howdy, this is a test text!";
+        } else if (x == 1)
+        {
+            text.text = "Hello world. Today is a fine day.";
+        }
+    }
+
+    public void ExitSite()
+    {
+        
+        exiting = true;
+    }
+}
