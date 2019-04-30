d1f050416958068099d80fb21b2cd91946880faf
Skamppi <46809473+Skamppi@users.noreply.github.com>
2019-04-30 10:50:23 +0300

    KameranVaihto ja FpsOptimizer

---
+++ Drio/Drio/Assets/MainFolder/Sekalaista/FpsOptimizer/FpsOptimizer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsOptimizer : MonoBehaviour
+{
+    public GameObject Scene2Objects;
+    public GameObject SecondGateObjects;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene2Objects.SetActive(false);
+        SecondGateObjects.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Scene2Objects.SetActive(true);
+            SecondGateObjects.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Scene2Objects.SetActive(false);
+            SecondGateObjects.SetActive(false);
+        }
+    }
+}
---
+++ Drio/Drio/Assets/MainFolder/Sekalaista/FreeCamera/CameraChanger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChanger : MonoBehaviour
+{
+    public Camera freeCamera;
+    public Camera normalCamera;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        normalCamera.enabled = true;
+        freeCamera.enabled = false;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+             
+            freeCamera.enabled = !freeCamera.enabled;
+            normalCamera.enabled = !normalCamera.enabled;
+            
+        }
+        }
+        
+    }
+
