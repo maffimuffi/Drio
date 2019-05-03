761436f49d8f247b2ca812ed431f1806a09b8273
Skamppi <46809473+Skamppi@users.noreply.github.com>
2019-05-03 11:08:41 +0300

    blocks

--- Drio/Drio/Assets/MainFolder/Sekalaista/SK_Scripts/BlockDown.cs
+++ Drio/Drio/Assets/MainFolder/Sekalaista/SK_Scripts/BlockDown.cs
@@ -7,12 +7,15 @@ public class BlockDown : MonoBehaviour
 
     bool isGoingDown;
     float down = -7.7f;
+    public GameObject otherBlock;
     Vector3 targetPosition;
+    Vector3 otherBlockTPos;
     // Start is called before the first frame update
     void Start()
     {
         isGoingDown = false;
         targetPosition = new Vector3(transform.position.x, -50f, transform.position.z);
+        otherBlockTPos = new Vector3(otherBlock.transform.position.x, -30f, otherBlock.transform.position.z);
     }
 
     // Update is called once per frame
@@ -22,6 +25,7 @@ public class BlockDown : MonoBehaviour
         {
             down -= Time.deltaTime;
             transform.position = transform.position = Vector3.MoveTowards(transform.position, targetPosition, 0.05f);
+            otherBlock.transform.position = Vector3.MoveTowards(otherBlock.transform.position, otherBlockTPos, 0.05f);
         }
     }
 
