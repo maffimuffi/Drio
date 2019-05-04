91e4022c2a4c9573a44a76f5f1f8e19de11f6ec5
Skamppi <46809473+Skamppi@users.noreply.github.com>
2019-05-04 12:36:31 +0300

    navmesh

--- Drio/Drio/Assets/MainFolder/Sekalaista/SK_Scripts/LastBowl.cs
+++ Drio/Drio/Assets/MainFolder/Sekalaista/SK_Scripts/LastBowl.cs
@@ -16,7 +16,7 @@ public class LastBowl : MonoBehaviour
     void Update()
     {
         
-        if (OpenDoor.trigger == 5)
+        if (OpenDoor.trigger == 6)
         {
             lastBowlLit = true;
             Debug.Log("TRUE");
